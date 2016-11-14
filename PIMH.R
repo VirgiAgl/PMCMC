db98d5c2a32a91623155c86d6f4eaa10c988aa7b
jackcarter23 <Jack@MacBook-Pro-74.local>
2016-11-14 13:31:58 +0000

    aesthetic changes and removed print(i)

--- PIMH.R
+++ PIMH.R
@@ -7,43 +7,40 @@ PIMH = function(n_iter,
                 observed_process){
   
   #initialisation
-  t=length(observed_process)     #number of observed step in times
-  n_acceptance=0
-  state_values = matrix(NA, nrow = N, ncol = n_iter+1 ) #store the state values at each step
-  lik_values = vector(length = n_iter+1 )               #store the lik value at each step
+  t = length(observed_process)     #number of observed step in times
+  n_acceptance = 0
+  state_values = matrix(NA, nrow = t, ncol = n_iter + 1) #store the state values at each step
+  lik_values = vector(length = n_iter + 1)               #store the lik value at each step
   
   # run an SMC for iteration 0
   smc_output = SMC(N = N, 
                     calculate_weight = calculate_weight, 
                     state_update = state_update, 
-                    observed_process = observed_process
-                    )
+                    observed_process = observed_process)
   
   index = sample(1:N, 1, prob = smc_output$weights_in_time[t, ]) #to check
-  proposed_x = smc_output$particles_in_time[,index]
+  proposed_x = smc_output$particles_in_time[, index]
   proposed_lik = smc_output$lik_in_time[t]
   
   # store the first two values
-  lik_values[1]=proposed_lik
-  state_values[,1]=proposed_x
+  lik_values[1] = proposed_lik
+  state_values[, 1] = proposed_x
   
   
   for (i in 1:n_iter){
-    print(i)
     # run an SMC for each iteration from 1 to n_iter
-    smc_output = SMC(N=N, 
-                      calculate_weight=calculate_weight, 
-                      state_update=state_update, 
-                      observed_process=observed_process
-                      )
+    smc_output = SMC(N = N, 
+                      calculate_weight = calculate_weight, 
+                      state_update = state_update, 
+                      observed_process = observed_process)
     
     # sample the path x1:xT to consider
     index = sample(1:N, 1, prob = smc_output$weights_in_time[t, ])
-    proposed_x = smc_output$particles_in_time[,index]
+    proposed_x = smc_output$particles_in_time[, index]
     proposed_lik = smc_output$lik_in_time[t]
     
     # compute the accepatance probability
-    acc_prob = min(c(1, proposed_lik/(lik_values[i]) ))
+    acc_prob = min(c(1, proposed_lik / lik_values[i]))
     
     # accept or reject the new value
     if(runif(1) < acc_prob){
@@ -51,15 +48,15 @@ PIMH = function(n_iter,
       lik_values[i+1] = proposed_lik
       n_acceptance = n_acceptance + 1
     } else {
-      state_values[, i+1] = state_values[,i] 
+      state_values[, i+1] = state_values[, i] 
       lik_values[i+1] = lik_values[i]
     }
     
     # compute the ratio of accepted values
-    acceptance_ratio = n_acceptance/n_iter
+    acceptance_ratio = n_acceptance / n_iter
   }
   
-  out=list(state_values = state_values,
+  out = list(state_values = state_values,
            lik_values = lik_values,
            acceptance_ratio = acceptance_ratio
            )
