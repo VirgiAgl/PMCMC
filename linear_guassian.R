3b1e62be85d7c0b6add433024a2f6ada840ef4c2
Virginia Aglietti <aglietti@toucanet.stats.ox.ac.uk>
2016-11-15 17:25:28 +0000

    change plots for the kalman filter

--- linear_guassian.R
+++ linear_guassian.R
@@ -92,7 +92,7 @@ linear_model_state_space = function(phi, mu, sigma, y_values, x_values) {
 }
 
 # Compute the Kalman filter       
-sp = linear_model_state_space(phi=phi, mu=mu, sigma=sigma ,y_values=observed_process_linear[1:20], x_values=latent_process_linear[1:20])
+sp = linear_model_state_space(phi=phi, mu=mu, sigma=sigma ,y_values=observed_process_linear[1:10], x_values=latent_process_linear[1:10])
 ans = fkf(a0 = sp$a0, P0 = sp$P0, dt = sp$dt, ct = sp$ct, Tt = sp$Tt,
            Zt = sp$Zt, HHt = sp$HHt, GGt = sp$GGt, yt = sp$yt)
 
@@ -102,7 +102,7 @@ predicted_exp_value = t(cbind(ans$at[1], ans$att))
 predicted_var = as.matrix(c(ans$Pt[1], as.vector(ans$Ptt)))
 x = seq(-8,8, by=0.01)
 values_to_plot = matrix(NA, nrow = length(x),ncol=length(predicted_exp_value))
-for (i in 1:(20+1)){
+for (i in 1:(10+1)){
   values_to_plot[,i] = dnorm(x, mean=predicted_exp_value[i,],sd=sqrt(predicted_var[i,]))
 }
 data_frame = as.data.frame(values_to_plot)
@@ -110,8 +110,8 @@ data_frame$x_values = x
 data_frame_melt = melt(data_frame, id=c("x_values"))
 n_values=as.matrix(table(data_frame_melt$variable))
 
-particle_means_df = data.frame(means = particle_mean_in_time[1:21])
-particle_means_df$variable = colnames(data_frame)[1:21]
+particle_means_df = data.frame(means = particle_mean_in_time[1:11])
+particle_means_df$variable = colnames(data_frame)[1:11]
 
 data_frame_melt$variable = factor(data_frame_melt$variable, levels=c('V1','V2','V3','V4','V5','V6','V7','V8','V9','V10','V11','V12','V13','V14','V15','V16','V17','V18','V19','V20','V21'))
 particle_means_df$variable = factor(particle_means_df$variable, levels = c('V1','V2','V3','V4','V5','V6','V7','V8','V9','V10','V11','V12','V13','V14','V15','V16','V17','V18','V19','V20','V21'))
@@ -133,6 +133,7 @@ plot1 = ggplot() +
         axis.text=element_text(size=12), 
         axis.title=element_text(size=12), 
         plot.title = element_text(lineheight=.8, face="bold"))
+ggsave("kalman_filter1.pdf")
 plot1
 
 
@@ -144,7 +145,7 @@ plot2 = ggplot() +
   geom_point(data = particle_means_df, aes(x=1, y = means, colour = "red", group = variable), size=3, alpha=0.6) +
   ggtitle("Predictives densities") +
   labs(x="Time index",y="State values") + 
-  xlim(0,1.5) +
+  xlim(0,2) +
   ylim(c(-0.5,3)) +
   theme(legend.position="none", plot.title = element_text(hjust = 0.5)) +
   facet_wrap(~variable, ncol = n_values[1] ) +
@@ -155,6 +156,7 @@ plot2 = ggplot() +
         axis.text=element_text(size=10), 
         axis.title=element_text(size=10), 
         plot.title = element_text(lineheight=.5, face="bold"))
+ggsave("kalman_filter2.pdf")
 plot2
 
 
