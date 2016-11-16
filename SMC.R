124cc455cb58d8a27b3d0e8f3a7622c1821e95ad
Tamar Loach <tamar.loach@spc.ox.ac.uk>
2016-11-16 16:01:41 +0000

    updated SMC function to use a log transformation - solves /0 e

--- SMC.R
+++ SMC.R
@@ -23,18 +23,20 @@ SMC = function(N, calculate_weight, state_update, observed_process, theta_state,
       particles_in_time[i,] = sapply(X = particles_in_time[i-1,], FUN = state_update, k=i, theta_state)#phi=phi, mu=mu, sigma=sigma)
     }
     
-    weight = calculate_weight(observed_val = observed_process[i], particles_vals = particles_in_time[i,], theta_obs)
-    
-    logl=logl+log(mean(exp(weight))) #log of the estimate of the likelihood
+    log_weight = calculate_weight(observed_val = observed_process[i], particles_vals = particles_in_time[i,], theta_obs)
+    log_weight_transformed = log_weight - max(log_weight)
+  
+    logl=logl+log(mean(exp(log_weight))) #log of the estimate of the likelihood
     #logl=logl+log(mean(weight)) #log of the estimate of the likelihood
     lik_in_time[i]=logl   #store the likelihood value
     
-
-    weight_norm = weight/sum(weight)  #normalize the weights
+    weight_transformed = exp(log_weight_transformed)
+    weight_norm = weight_transformed/sum(weight_transformed)  #normalize the weights
     weights_in_time[i,] = weight_norm #store the weights
     
     # Store the value of the mean and the variance (before resampling) 
-    particle_mean_in_time[i,]=sum(weight_norm*particles_in_time[i,])                                    # expected value of a random var
+    #particle_mean_in_time[i,]=sum(weight_norm*particles_in_time[i,])                                    # expected value of a random var
+    particle_mean_in_time[i,]=sum(weight_norm*particles_in_time[i,])  
     particle_variance_in_time[i,]=sum(weight_norm*particles_in_time[i,]^2)-particle_mean_in_time[i,]^2  # variance of a random var
     
     # resample (not for the last step)
